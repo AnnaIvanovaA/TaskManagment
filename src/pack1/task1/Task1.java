2d8bb84d35a4647921362e02d039589f4e096a61
Anna.Ivanova <anna.ivanova@jetbrains.com>
2018-09-06 15:52:49 +0300

    Task1

--- src/pack1/task1/Task1.java
+++ src/pack1/task1/Task1.java
@@ -3,5 +3,12 @@ package pack1.task1;
 public class Task1 {
     public static void main(String[] args) {
         System.out.println("smth fot task1");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("some other changes");
+        }
+        System.out.println("qqqq");
+
+
     }
 }
---
+++ src/pack1/task1/Task11.java
@@ -0,0 +1,7 @@
+package pack1.task1;
+
+public class Task11 {
+    public static void main(String[] args) {
+        System.out.println("Add new file for task1 changelist");
+    }
+}
