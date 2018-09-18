925b05c2f73f41e7d67436c6c77c2414f9cfe708
Anna.Ivanova <anna.ivanova@jetbrains.com>
2018-09-18 16:44:23 +0300

    add one new line

--- src/pack1/task1/Task1.java
+++ src/pack1/task1/Task1.java
@@ -3,5 +3,6 @@ package pack1.task1;
 public class Task1 {
     public static void main(String[] args) {
         System.out.println("smth fot task1");
+        System.out.println("add smth new");
     }
 }
