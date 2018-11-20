1ad45b9a3d01b4320cadb7ae1a71568c4ff7299b
anna.ivanova <anna.ivanova@jetbrains.com>
2018-11-20 15:29:49 +0300

    task2

---
+++ src/pack2/task2/OriginTask2.java
@@ -0,0 +1,4 @@
+package pack2.task2;
+
+public class OriginTask2 {
+}
--- src/pack2/task2/Task2.java
+++ src/pack2/task2/Task2.java
@@ -1,4 +1,11 @@
 package pack2.task2;
 
 public class Task2 {
+    public static void main(String[] args) {
+        System.out.println("task2 changes");
+
+        String str = "smth new for task2";
+        System.out.println("qq");
+        System.out.println("dmrk");
+    }
 }
