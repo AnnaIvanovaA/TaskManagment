97c547e864d9985113ef1dc3e812fcfe6efe110a
Anna.Ivanova <anna.ivanova@jetbrains.com>
2020-05-07 13:55:06 +0300

    TaskManagment-1 First github issue

---
+++ src/jirapack/JiraCloudTest.java
@@ -0,0 +1,28 @@
+package jirapack;
+
+public class JiraCloudTest {
+    public static void main(String[] args) {
+        System.out.println("");
+
+        System.out.println("");
+
+        new JiraCloudTest().method1();
+        new JiraCloudTest().method2();
+
+
+        new JiraCloudTest().method1();
+        new JiraCloudTest().method2();
+        new JiraCloudTest().method2();
+        new JiraCloudTest().method1();
+        new JiraCloudTest().method2();
+
+    }
+
+    public void method1(){
+        System.out.println('s');
+    }
+
+    public void method2(){
+        System.out.println("eqweqweqeq");
+    }
+}
---
+++ src/pack2/task2/TimeTrackingTest.java
@@ -0,0 +1,7 @@
+package pack2.task2;
+
+public class TimeTrackingTest {
+    public static void main(String[] args) {
+
+    }
+}
